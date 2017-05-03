d93103ca49375caa6ce9528952d627ec37ac1d8d
010011100010 <ll3305@nyu.edu>
2017-05-03 15:36:06 -0400

    Started the project. Created blue, red, yellow, and black materials and prefabs using them. The art management which paints on the canvas is working now.

---
+++ 3D Mondrian/Assets/Scripts/automaticMondrian.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class automaticMondrian : MonoBehaviour {
+	private Vector3 spawnPos;
+	public GameObject[] cubes;
+	public int mondrianess = 100;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Paint ();
+		}
+	}
+
+	void Paint () {
+		for (int i = 0; i < mondrianess; i++) {
+			spawnPos = new Vector3 (Random.Range (0, 100), Random.Range (0, 100), Random.Range (0, 100));
+			Instantiate (cubes [Random.Range (0, cubes.Length)], spawnPos, Quaternion.identity);
+		}
+	}
+}
